81d577de290a7092d872956f422202cde724d48f
GitHubDiom <867320164@qq.com>
2022-04-19 08:30:37 +0000

    Environmental modification

--- copy-files.sh
+++ copy-files.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-cp /home/ubuntu/repos/hops/hops-metadata-dal-impl-ndb/target/hops-metadata-dal-impl-ndb-3.2.0.3-SNAPSHOT.jar /home/ubuntu/repos/openwhisk-runtime-java/core/java8/hops-metadata-dal-impl-ndb-3.2.0.3-SNAPSHOT.jar
-cp /home/ubuntu/repos/hops/hadoop-common-project/hadoop-common/target/hadoop-common-3.2.0.3-SNAPSHOT.jar /home/ubuntu/repos/openwhisk-runtime-java/core/java8/libs/hadoop-common-3.2.0.3-SNAPSHOT.jar
+cp /home/vagrant/hops/hops-metadata-dal-impl-ndb/target/hops-metadata-dal-impl-ndb-3.2.0.3-SNAPSHOT.jar /home/vagrant/openwhisk-runtime-java/core/java8/hops-metadata-dal-impl-ndb-3.2.0.3-SNAPSHOT.jar
+cp /home/vagrant/hops/hadoop-common-project/hadoop-common/target/hadoop-common-3.2.0.3-SNAPSHOT.jar /home/vagrant/openwhisk-runtime-java/core/java8/libs/hadoop-common-3.2.0.3-SNAPSHOT.jar
+cp /home/vagrant/hops/hadoop-hdfs-project/hadoop-hdfs/src/main/conf/ndb-config.properties /home/vagrant/openwhisk-runtime-java/core/java8/ndb-config.properties
